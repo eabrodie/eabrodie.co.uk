913e42450338cbe1e261375388e61dd83ce9e9f4
Eleanor Brodie <eabrodie@hotmail.co.uk>
2016-02-13 15:38:46 +0000

    Updated second page

--- server.js
+++ server.js
@@ -8,4 +8,8 @@ app.get('/', function (req, res, next) {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/second.html', function (req, res, next) {
+  res.sendFile(__dirname + '/second.html');
+});
+
 module.exports = app.listen(3000);
